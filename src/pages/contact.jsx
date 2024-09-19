71ec4e2da2ecea6c9a5a64860bbfa0da0bc4120f
Bori Kokeny <bori@minerali.hu>
2024-09-19 13:03:13 +0200

    Sets html for contact form

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -1,6 +1,40 @@
 export default function Contact() {
   return (
-    <div id="main-container" className="container mx-auto">
-       <div>Abutbtubtu</div>
-    </div>)
+    <div>
+      <h2 className="my-10 text-center font-semibold leading-7 text-gray-900">Contact Us</h2>
+      <form className="flex justify-center">
+        <div className="w-96 space-y-6">
+          <div className="">
+            <div className="relative mt-2">
+              <div className="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3">
+               <span className="text-gray-500 sm:text-sm">Full Name</span>
+              </div>
+              <input type="text" name="first-name" id="first-name" autocomplete="given-name" className="block w-full rounded-none border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+            </div>
+            <div className="relative mt-2">
+              <div className="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3">
+               <span className="text-gray-500 sm:text-sm">Subject</span>
+              </div>
+              <input type="text" name="last-name" id="last-name" autocomplete="family-name" className="block w-full rounded-none border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+            </div>
+            <div className="relative mt-2">
+              <div className="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3">
+               <span className="text-gray-500 sm:text-sm">Email</span>
+              </div>
+              <input id="email" name="email" type="email" autocomplete="email" className="block w-full rounded-none border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+            </div>
+            <div className="relative mt-2">
+              <div className="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3">
+               <span className="text-gray-500 sm:text-sm">Message</span>
+              </div>
+              <textarea id="about" name="about" rows="3" className="block w-full rounded-none border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"></textarea>
+            </div>
+          </div>
+          <div className="mt-6 flex items-center justify-center gap-x-6">
+           <button type="submit" className="w-64 rounded-none bg-red-600 px-3 py-2 text-base font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Send</button>
+          </div>
+        </div>
+      </form>
+    </div>
+    )
 }
