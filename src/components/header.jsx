8d4a59e45bfef204711c2edf879e4d7865da51ef
Bori Kokeny <bori@minerali.hu>
2024-09-20 13:23:24 +0200

    Adds images to header

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,16 +1,21 @@
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
+import Logo from "../images/Logo.jpg";
+import Cart from "../images/Cart.jpg";
 
 export default function Header() {
   return (
     <header className="shadow-md">
     <div className="container mx-auto flex justify-between items-center p-4">
       <div className="text-lg font-bold">
-      <Link to="/" className="hover:text-gray-200">Jim's Convo</Link>
+      <Link to="/" className="hover:text-gray-200"><img src={Logo} className="w-20"/></Link>
       </div>
       <nav className="space-x-4">
         <Link to="/shop" className="hover:text-gray-200">Shop</Link>
         <Link to="/contact" className="hover:text-gray-200">Contact</Link>
       </nav>
+      <div>
+        <Link to="/checkout" className=""><img src={Cart} className="w-5"/></Link>
+      </div>
     </div>
   </header>
   )
