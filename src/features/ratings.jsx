de545985329d30f87479553e7f256cce7abb17fa
Bori Kokeny <bori@minerali.hu>
2024-09-26 13:08:28 +0200

    Stages shop-page

---
+++ src/features/ratings.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { TiStarFullOutline, TiStarHalfOutline, TiStarOutline } from "react-icons/ti";
+
+const Ratings = ({ rating }) => {
+  const stars = Array.from({ length: 5 }, (element, index) =>{
+    let number = index + 0.5;
+    return (
+      <span key={index} rating={index}>
+        {rating > index + 1 ? (
+          <TiStarFullOutline className="text-yellow-500" />
+        ) : rating > number ? (
+          <TiStarHalfOutline className="text-yellow-500" />
+        ) : (
+          <TiStarOutline className="text-yellow-500" />
+        )}
+      </span>
+    );
+  });
+  return (
+    <div className="flex justify-start gap-2 items-center">{stars}</div>
+  );
+};
+
+export default Ratings
--- src/pages/shop.jsx
+++ src/pages/shop.jsx
@@ -1,36 +1,76 @@
 import React, { useEffect, useState, Link } from "react";
+import Ratings from "../features/ratings";
 
 const url = "https://v2.api.noroff.dev/online-shop";
 
 function Shop() {
-  
-
   const [products, setProducts] = useState([]);
 
-  useEffect(() => {
-    async function getData() {
-      const response = await fetch(url);
-      const json = await response.json();
-      setProducts(json.data);
-    }
-    getData();
-  }, []);
+    useEffect(() => {
+      async function getData() {
+        const response = await fetch(url);
+        const json = await response.json();
+        setProducts(json.data);
+      }
+      getData();
+    }, []);
+  const productList = products.map((product) => {
+    const { id, title, description, price, discountedPrice, image, rating } = product;
+    const gotDiscount = discountedPrice < price;
 
+    return (
+      <div className="">
+        <img src={image.url} className="object-cover mt-6 h-48 w-96" alt={title} />       
+        <h2 className="font-bold mt-2">{title}</h2>
+        {/* <p>{description}</p> */}
+        {/* <span><TiStarFullOutline className="text-yellow-500" /></span> */}
+       <Ratings rating={rating} />
+      
+        {gotDiscount ? (<div className="mb-3">
+          <span className="text-red-700 font-bold pe-2">{discountedPrice} kr</span>
+          <span className="line-through text-sm">{price} kr</span>
+        </div>
+         ) : (
+          <p className="text-red-700 font-bold mb-3">{discountedPrice} kr</p>
+         )}
+        <button type="button" className="rounded-none bg-white px-2.5 py-1.5 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50">View product</button>
+      </div>
+    )
+  });
   return (
-    <div>
-        {products.map((product) => (
-          <div>   
-            <img src={product.image.url} className="object-cover mt-6 h-48 w-96" alt={product.title} />       
-            <h2 className="font-bold">{product.title}</h2>
-            {/* <p>{product.description}</p> */}
-            <p>{product.price} kr</p>
-            <p className="text-red-700 font-bold">{product.discountedPrice} kr</p>
-            
-            {/* <Link to={`/product/${product.id}`}>View Product</Link> */}
-          </div>
-        ))}
-    </div>
-  );
+    <section className="grid grid-cols-4 gap-4">{productList}</section>
+  )
 }
 
 export default Shop
+// function Shop() { 
+
+//   const [products, setProducts] = useState([]);
+
+//   useEffect(() => {
+//     async function getData() {
+//       const response = await fetch(url);
+//       const json = await response.json();
+//       setProducts(json.data);
+//     }
+//     getData();
+//   }, []);
+
+//   return (
+//     <div>
+//         {products.map((product) => (
+//           <div>   
+//             <img src={product.image.url} className="object-cover mt-6 h-48 w-96" alt={product.title} />       
+//             <h2 className="font-bold">{product.title}</h2>
+//             {/* <p>{product.description}</p> */}
+//             <p>{product.price} kr</p>
+//             <p className="text-red-700 font-bold">{product.discountedPrice} kr</p>
+            
+//             {/* <Link to={`/product/${product.id}`}>View Product</Link> */}
+//           </div>
+//         ))}
+//     </div>
+//   );
+// }
+
+// export default Shop
