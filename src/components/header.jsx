e8c2eec4055663cbb48d4e37dd0c4189fb9a81b6
Bori Kokeny <bori@minerali.hu>
2024-10-03 13:05:27 +0200

    Resizes logo

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -7,7 +7,7 @@ export default function Header() {
     <header className="shadow-md">
     <div className="container mx-auto flex justify-between items-center p-4">
       <div className="text-lg font-bold">
-      <Link to="/" className="hover:text-gray-200"><img src={Logo} className="w-20"/></Link>
+      <Link to="/" className="hover:text-gray-200"><img src={Logo} className="w-1/3"/></Link>
       </div>
       <nav className="space-x-4">
         <Link to="/shop" className="hover:text-gray-200">Shop</Link>
