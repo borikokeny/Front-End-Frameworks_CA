f78d496462b4e86704e38e5321c1867ec79995b4
Bori Kokeny <bori@minerali.hu>
2024-10-03 13:09:55 +0200

    Sets hook on api

--- src/pages/shop.jsx
+++ src/pages/shop.jsx
@@ -7,16 +7,17 @@ import { Link } from "react-router-dom";
 
 function Shop() {
   const url = "https://v2.api.noroff.dev/online-shop";
-  const [products, setProducts] = useState([]);
-
-    useEffect(() => {
-      async function getData() {
-        const response = await fetch(url);
-        const json = await response.json();
-        setProducts(json.data);
-      }
-      getData();
-    }, []);
+  const { data } = useApi(url);
+  // const [products, setProducts] = useState([]);
+  const products = data;
+    // useEffect(() => {
+    //   async function getData() {
+    //     const response = await fetch(url);
+    //     const json = await response.json();
+    //     setProducts(json.data);
+    //   }
+    //   getData();
+    // }, []);
 
   const productList = products.map((product) => {
     const { id, title, description, price, discountedPrice, image, rating } = product;
@@ -24,17 +25,19 @@ function Shop() {
  
     return (
       <div className="">
+        <Link to={`/product/${product.id}`} key={product.id}>
         <img src={image.url}  alt={title} className="object-cover mt-6 h-48 w-96"/>       
         <h2 className="font-bold mt-2">{title}</h2>
         <Ratings rating={rating} />
         {gotDiscount ? (<div className="mb-3">
-          <span className="text-red-700 font-bold pe-2">{discountedPrice} kr</span>
-          <span className="line-through text-sm">{price} kr</span>
+          <span className="text-red-700 font-bold pe-2">{discountedPrice} NOK</span>
+          <span className="line-through text-sm">{price} NOK</span>
         </div>
          ) : (
-          <p className="text-red-700 font-bold mb-3">{discountedPrice} kr</p>
+          <p className="text-red-700 font-bold mb-3">{discountedPrice} NOK</p>
          )}
         <Link to={`/product/${product.id}`} key={product.id} className="rounded-none bg-white px-2.5 py-1.5 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50">View product</Link>
+        </Link>
       </div>
     )
   });
