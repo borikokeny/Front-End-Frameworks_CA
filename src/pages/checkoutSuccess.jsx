f23c77b6f98b3d10ab0f16b35282bc6639ecaf8b
Bori Kokeny <bori@minerali.hu>
2024-10-09 09:18:30 +0200

    Adds modal to checkoutsuccesspage

--- src/pages/checkoutSuccess.jsx
+++ src/pages/checkoutSuccess.jsx
@@ -1,16 +1,45 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { BsFillCheckCircleFill } from "react-icons/bs";
 
 function CheckoutSuccess() {
   return (
-    <div id="main-container" className="container mx-auto">
-      <main className="text-center"> 
-        <h1>
-          Thank you for your purchase!
-        </h1>
-        <Link to="/shop" className="mt-11 rounded-none bg-red-600 text-white w-1/4 px-2.5 py-1.5 text-sm font-semibold shadow-sm ring-1 ring-inset hover:bg-red-500">Continue Shopping</Link>
-      </main>
+<div className="relative z-10" aria-labelledby="modal-title" role="dialog" aria-modal="true">
+  <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" aria-hidden="true"></div>
+  <div className="fixed inset-0 z-10 w-screen overflow-y-auto">
+    <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
+      <div className="relative transform overflow-hidden bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
+        <div className="bg-white px-4 pb-4 pt-5 sm:p-6 sm:pb-4">
+          <div className="sm:flex sm:items-start">
+          <div className="mx-auto flex h-12 w-12 flex-shrink-0 items-center justify-center rounded-full bg-red-100 sm:mx-0 sm:h-10 sm:w-10">
+              <BsFillCheckCircleFill className="h-24 w-24 text-red-600"/>
+            </div>
+            <div className="mt-3 text-center sm:ml-4 sm:mt-0 sm:text-left">
+              <h3 className="text-base font-semibold leading-6 text-gray-900" id="modal-title">Thank you for your purchase!</h3>
+              <div className="mt-2">
+                <p className="text-sm text-gray-500">We'll send you a shipping informaton email as soon as your order ships.</p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="bg-gray-50 px-4 py-3 sm:flex sm:flex-col sm:px-6">
+          <Link to="/shop" className="inline-flex w-full justify-center rounded-none bg-red-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-red-500 sm:ml-3 sm:w-auto">Continue Shopping</Link>
+        </div>
+      </div>
     </div>
+  </div>
+</div>
+
+
+
+    // <div id="main-container" className="container mx-auto">
+    //   <main className="text-center"> 
+    //     <h1>
+    //       Thank you for your purchase!
+    //     </h1>
+    //     <Link to="/shop" className="mt-11 rounded-none bg-red-600 text-white w-1/4 px-2.5 py-1.5 text-sm font-semibold shadow-sm ring-1 ring-inset hover:bg-red-500">Continue Shopping</Link>
+    //   </main>
+    // </div>
   )
 }
 
